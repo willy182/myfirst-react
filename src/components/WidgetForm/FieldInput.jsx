49872ecb283f276feb7a2d0e3e014735ce4175dd
willy182 <willykristianto89@gmail.com>
2016-10-31 20:39:10 +0700

    first commit

---
+++ src/components/WidgetForm/FieldInput.jsx
@@ -0,0 +1,46 @@
+import React, {Component} from 'react';
+
+export default class FieldInput extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      valid: false,
+      errorMessage: ""
+    };
+  }
+  handleChange (s){
+    let u = s.target.value
+    console.log(u)
+    let valid = this.props.validate(u);
+    this.validation(event.target.value, valid);
+  }
+  validation (value, valid) {
+    if (typeof valid === 'undefined') {
+      valid = true;
+    }
+    
+    var message = "";
+    
+    //we know how to validate text fields based on information passed through props
+    if (!valid) {
+      message = 'Input is not invalid';
+      valid = false;
+    }
+
+    this.setState({
+      valid: valid,
+      errorMessage: message,
+    });
+  }
+  render () {
+    return (
+      <div>
+        <input
+          placeholder={this.props.text}
+          type={this.props.type}
+          name={this.props.name}
+          onChange={this.handleChange.bind(this)} /><span>{this.state.errorMessage}</span>
+      </div>
+    );
+  }
+}
---
+++ src/components/WidgetForm/Form.jsx
@@ -0,0 +1,42 @@
+import React, {Component} from 'react';
+import FieldInput from './FieldInput';
+
+export default class Form extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: "",
+      email: "",
+      password: ""
+    };
+  }
+  validateEmail (value) {
+    // regex from http://stackoverflow.com/questions/46155/validate-email-address-in-javascript
+    var re = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/;
+    return re.test(value);
+  }
+  handleSubmit (e) {
+    console.log(e)
+    let u = this.state.username
+    if(u === ''){
+      alert('semua inputan harus diisi');
+    }
+    else{
+      alert(u);
+      // $.post(Config.BASE_URL+'/addpost/', {userId:store.get('user').userId, status:this.state.post}, function(mypost){
+
+      // });
+    }
+    e.preventDefault();
+  }
+  render () {
+    return (
+      <form onSubmit={this.handleSubmit.bind(this)}>
+        <FieldInput name="username" type="text" text="your username" validate="" />
+        <FieldInput name="email" type="text" text="your email" validate={this.validateEmail.bind(this)} />
+        <FieldInput name="password" type="password" text="your password" validate="" />
+        <input type="submit" value="Submit" />
+      </form>
+    );
+  }
+}
