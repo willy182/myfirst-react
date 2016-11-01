edcb3f63b958580c25af46c078952e7fdee2a249
willy182 <willykristianto89@gmail.com>
2016-11-01 20:48:06 +0700

    hapus validation

--- src/components/WidgetForm/FieldInput.jsx
+++ src/components/WidgetForm/FieldInput.jsx
@@ -4,32 +4,13 @@ export default class FieldInput extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      valid: false,
-      errorMessage: ""
+      nilai: ""
     };
   }
-  handleChange (s){
-    let u = s.target.value
-    let valid = this.props.validate(u);
-    this.validation(event.target.value, valid);
-  }
-  validation (value, valid) {
-    if (typeof valid === 'undefined') {
-      valid = true;
-    }
-    
-    var message = "";
-    
-    //we know how to validate text fields based on information passed through props
-    if (!valid) {
-      message = 'Input is not invalid';
-      valid = false;
-    }
-
+  handleChange (e){
     this.setState({
-      valid: valid,
-      errorMessage: message,
-    });
+      nilai: e.target.value
+    })
   }
   render () {
     return (
@@ -38,7 +19,8 @@ export default class FieldInput extends Component {
           placeholder={this.props.text}
           type={this.props.type}
           name={this.props.name}
-          onChange={this.handleChange.bind(this)} /><span>{this.state.errorMessage}</span>
+          onChange={this.handleChange.bind(this)}
+          value={this.state.nilai} /><span>{this.state.errorMessage}</span>
       </div>
     );
   }
--- src/components/WidgetForm/Form.jsx
+++ src/components/WidgetForm/Form.jsx
@@ -2,19 +2,6 @@ import React, {Component} from 'react';
 import FieldInput from './FieldInput';
 
 export default class Form extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      username: "",
-      email: "",
-      password: ""
-    };
-  }
-  validateEmail (value) {
-    // regex from http://stackoverflow.com/questions/46155/validate-email-address-in-javascript
-    var re = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/;
-    return re.test(value);
-  }
   handleSubmit (e) {
     console.log(e)
     e.preventDefault();
@@ -22,9 +9,9 @@ export default class Form extends Component {
   render () {
     return (
       <form onSubmit={this.handleSubmit.bind(this)}>
-        <FieldInput name="username" type="text" text="your username" validate="" />
-        <FieldInput name="email" type="text" text="your email" validate={this.validateEmail.bind(this)} />
-        <FieldInput name="password" type="password" text="your password" validate="" />
+        <FieldInput name="username" type="text" text="your username" />
+        <FieldInput name="email" type="text" text="your email" />
+        <FieldInput name="password" type="password" text="your password" />
         <input type="submit" value="Submit" />
       </form>
     );
