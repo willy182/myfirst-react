7d6186af98705dce75288234dd3306f35e664f03
willy182 <willykristianto89@gmail.com>
2016-11-02 20:37:20 +0700

    remove component FieldInput

--- src/components/WidgetForm/Form.jsx
+++ src/components/WidgetForm/Form.jsx
@@ -1,19 +1,58 @@
 import React, {Component} from 'react';
-import FieldInput from './FieldInput';
 
 export default class Form extends Component {
-  handleSubmit (e) {
-    console.log(e)
-    e.preventDefault();
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: "",
+      email: "",
+      password: ""
+    };
+  }
+  handleSubmit (event){
+    let u = this.state.username
+    let e = this.state.email
+    let p = this.state.password
+    // $.post('http://192.168.9.231:1337/users', {u, e, p}, function(callback){
+    //   //action
+    // });
+    console.log(u + '|' + e + '|' + p)
+    event.preventDefault();
+  }
+  handleUsername (e) {
+    this.setState({username: e.target.value});
+  }
+  handleEmail (e) {
+    this.setState({email: e.target.value});
+  }
+  handlePassword (e) {
+    this.setState({password: e.target.value});
   }
   render () {
+    const divStyle = {
+      padding: 5,
+      width: 295
+    }
+    const inputStyle = {
+      float: "left",
+      flex: 1,
+      margin: 5,
+      height: 34
+    }
+    const buttonStyle = {
+      float: "left",
+      marginRight: 0,
+      width: 120,
+      margin: 5,
+      padding: 5
+    }
     return (
-      <form onSubmit={this.handleSubmit.bind(this)}>
-        <FieldInput name="username" type="text" text="your username" />
-        <FieldInput name="email" type="text" text="your email" />
-        <FieldInput name="password" type="password" text="your password" />
-        <input type="submit" value="Submit" />
-      </form>
+      <div style={divStyle}>
+        <input onChange={this.handleUsername.bind(this)} type="text" placeholder="your username" value={this.state.username} style={inputStyle} />
+        <input onChange={this.handleEmail.bind(this)} type="text" placeholder="your email" value={this.state.email} style={inputStyle} />
+        <input onChange={this.handlePassword.bind(this)} type="password" placeholder="your password" value={this.state.password} style={inputStyle} />
+        <button style={buttonStyle} onClick={this.handleSubmit.bind(this)}>Submit</button>
+      </div>
     );
   }
 }
